e3d4c47f3b65b1122fe95c70bec69a1f86e017b2
Rafael Pires Rozzatti <140448135+rafarozzatti@users.noreply.github.com>
2024-10-22 14:44:54 -0300

    Querie13 criada

---
+++ Queries para Responder os 20 Itens da Atividade de Álgebra Relacional/Querie13.sql
@@ -0,0 +1,8 @@
+/*Encontre os nomes dos usuários que têm playlists que incluem a música 'Bohemian Rhapsody'.*/
+
+SELECT u.nome
+FROM Usuarios u
+JOIN Playlists p ON u.id = p.usuario_id
+JOIN Playlist_Musicas pm ON p.id = pm.playlist_id
+JOIN Musicas m ON pm.musica_id = m.id
+WHERE m.titulo = 'Bohemian Rhapsody';
