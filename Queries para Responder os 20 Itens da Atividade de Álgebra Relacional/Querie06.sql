a997039909aa439cfda8c0883f1984dde48b0cdb
Rafael Pires Rozzatti <140448135+rafarozzatti@users.noreply.github.com>
2024-10-22 14:35:55 -0300

    Querie06 criada

---
+++ Queries para Responder os 20 Itens da Atividade de Álgebra Relacional/Querie06.sql
@@ -0,0 +1,9 @@
+/*Liste os usuários que criaram playlists que contêm músicas do disco 'Abbey Road'.*/
+
+SELECT u.nome
+FROM Usuarios u
+JOIN Playlists p ON u.id = p.usuario_id
+JOIN Playlist_Musicas pm ON p.id = pm.playlist_id
+JOIN Musicas m ON pm.musica_id = m.id
+JOIN Discos d ON m.disco_id = d.id
+WHERE d.titulo = 'Abbey Road';
